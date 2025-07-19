29dc2e5cb5f9c6b38791f279c594607e72171ab4
csm <141980543+csm009177@users.noreply.github.com>
2025-07-20 00:03:14 +0900

    객체에서 각각 꺼내쓰기

--- t1/test.py
+++ t1/test.py
@@ -1,15 +1,10 @@
 item_one = "사과"
 item_two = "우유"
-아이템덩어리 = {
-    'item3':'양파',
-    'item4':'마늘',
-    'item5':'대파',
-    'item6':'고추',    
-    'item7':'후추',    
-}
+장바구니 = {'item3':'양파','item4':'마늘','item5':'대파','item6':'고추','item7':'후추'}
 
 file_name = "shopping" + ".txt"
-file = open("./t1/" + file_name, "w")
-file.write("구매할 것들:\n1. " + item_one + "\n2. " + item_two)
+file = open("./t1/" + file_name, "w", encoding='utf-8')
+file.write("구매할 것들:\n1. " + item_one + "\n2. " + item_two + "\n")
+file.write("쥬스재료:" +"\n"+ 장바구니["item3"] +"\n"+ 장바구니['item4']+"\n"+ 장바구니['item5']+"\n"+ 장바구니['item6']+"\n"+ 장바구니['item7'])
 file.close()
 print("쇼핑 리스트가 저장되었습니다!")
