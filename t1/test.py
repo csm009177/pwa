c57b80d27e0b4133e83d9d7652e64ea6ff43e351
csm <csm009177@gmail.com>
2025-07-19 22:45:01 +0900

    copy t1

---
+++ t1/test.py
@@ -0,0 +1,7 @@
+item_one = "사과"
+item_two = "우유"
+file_name = "shopping" + ".txt"
+file = open(file_name, "w")
+file.write("구매할 것들:\n1. " + item_one + "\n2. " + item_two)
+file.close()
+print("쇼핑 리스트가 저장되었습니다!")
