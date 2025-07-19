2d3bd894c8b37135d8ffbaf08fa4558abc16474b
csm <141980543+csm009177@users.noreply.github.com>
2025-07-19 23:44:58 +0900

    객체생성

--- t1/test.py
+++ t1/test.py
@@ -1,5 +1,13 @@
 item_one = "사과"
 item_two = "우유"
+아이템덩어리 = {
+    'item3':'양파',
+    'item4':'마늘',
+    'item5':'대파',
+    'item6':'고추',    
+    'item7':'후추',    
+}
+
 file_name = "shopping" + ".txt"
 file = open("./t1/" + file_name, "w")
 file.write("구매할 것들:\n1. " + item_one + "\n2. " + item_two)
