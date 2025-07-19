f31f154726d4a8a789931280126fffb0e31388a1
csm <141980543+csm009177@users.noreply.github.com>
2025-07-20 00:03:59 +0900

    객체 요소를 반복문돌려서 출력

--- t1/test.py
+++ t1/test.py
@@ -6,5 +6,10 @@ file_name = "shopping" + ".txt"
 file = open("./t1/" + file_name, "w", encoding='utf-8')
 file.write("구매할 것들:\n1. " + item_one + "\n2. " + item_two + "\n")
 file.write("쥬스재료:" +"\n"+ 장바구니["item3"] +"\n"+ 장바구니['item4']+"\n"+ 장바구니['item5']+"\n"+ 장바구니['item6']+"\n"+ 장바구니['item7'])
+
+file.write("장바구니에 물건 전부 꺼내기 : \n ")
+for key, value in 장바구니.items():
+    file.write(f"{value}\n")
+    
 file.close()
 print("쇼핑 리스트가 저장되었습니다!")
