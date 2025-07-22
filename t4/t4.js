90246cae934c49ef24a33840edd6520549dd045c
csm <csm009177@gmail.com>
2025-07-22 14:10:56 +0900

    copy t4

--- t4/t4.js
+++ t4/t4.js
@@ -19,7 +19,8 @@ var addressComponents = [
 
 var rangeValue = {
     start: 0,
-    end: addressComponents.length // 랭쓰(length)라고 부르는 요녀석은 어떤 녀석일까요?
+    // 랭쓰(length)라고 부르는 요녀석은 어떤 녀석일까요?
+    end: addressComponents.length 
 };
 
 console.log("=== 기본 예제: 주소 구성 요소 출력 ===");
@@ -27,10 +28,15 @@ for (var index = rangeValue["start"]; index < rangeValue["end"]; index++) {
     console.log((index + 1) + "번째 구성 요소: " + addressComponents[index]);
 }
 
-var fullAddress = ""; // 이것은 단순한 '빈 문자열 선언'일 뿐이지만, 직접 자유롭게 만드는 것 까지 활용능력은 매우 길게 생각하고 다루는 것이므로, 편하게 확인하시기 바랍니다.
+// 이것은 단순한 '빈 문자열 선언'일 뿐이지만, 
+// 직접 자유롭게 만드는 것 까지 활용능력은 매우 길게 생각하고 다루는 것이므로, 
+// 편하게 확인하시기 바랍니다.
+// 매우 어려운 테크닉이므로, '이런게 있구나 하고 눈도장 정도만 찍읍시다!
+var fullAddress = ""; 
 for (var i = rangeValue["start"]; i < rangeValue["end"]; i++) {
     if (i > 0) {
-        fullAddress += " "; // 매우 어려운 테크닉이므로, '이런게 있구나 하고 눈도장 정도만 찍읍시다!
+        
+        fullAddress += " "; 
     }
     fullAddress =  fullAddress + addressComponents[i];
     console.log((i + 1) + "단계: " + fullAddress);
