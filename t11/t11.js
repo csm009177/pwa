6567fd99e453965babd15a0dee3e0e88f1f2ebba
csm <csm009177@gmail.com>
2025-07-31 14:18:50 +0900

    copy - 511.js

---
+++ t11/t11.js
@@ -0,0 +1,212 @@
+
+const daejeonLine1 = {
+  stations: ["판암", "신흥", "대동", "대전역", "중앙로", "중구청", "서대전네거리", "오룡", "용문", "탄방", "시청", "정부청사", "갈마", "월평", "갑천"],
+  fare: 1400,
+  openYear: 2006
+};
+
+function exampleOne() {
+  function visitStations(stations, callback) {
+    for (let i = 0; i < stations.length; i++) {
+      callback(stations[i], i);
+    }
+  }
+  
+  visitStations(daejeonLine1.stations, function(station, index) {
+    console.log(index + 1 + "번째 역: " + station);
+  });
+}
+
+function exampleTwo() {
+  function findStations(stations, callback) {
+    let result = [];
+    let resultIndex = 0;
+    
+    for (let i = 0; i < stations.length; i++) {
+      if (callback(stations[i])) {
+        result[resultIndex] = stations[i];
+        resultIndex++;
+      }
+    }
+    return result;
+  }
+  
+  const shortNames = findStations(daejeonLine1.stations, function(station) {
+    return station.length <= 2;
+  });
+  
+  console.log("2글자 이하 역명:", shortNames);
+}
+
+function exampleThree() {
+  let totalDistance = 0;
+  
+  function calculateDistance(stationCount) {
+    let averageDistance = 1.5;
+    totalDistance = (stationCount - 1) * averageDistance;
+    return totalDistance;
+  }
+  
+  console.log("총 거리:", calculateDistance(daejeonLine1.stations.length) + "km");
+  console.log("전역변수 totalDistance:", totalDistance);
+}
+
+function exampleFour() {
+  function fareCalculator() {
+    let baseFare = daejeonLine1.fare;
+    
+    function childFare() {
+      return baseFare * 0.5;
+    }
+    
+    function youthFare() {
+      return baseFare * 0.8;
+    }
+    
+    return {
+      child: childFare(),
+      youth: youthFare(),
+      adult: baseFare
+    };
+  }
+  
+  const fares = fareCalculator();
+  console.log("요금 체계:", fares);
+}
+
+function exampleFive() {
+  function calculateFare(base, passengers, callback) {
+    return callback(base, passengers);
+  }
+  
+  const multiply = calculateFare(daejeonLine1.fare, 5, function(fare, count) {
+    return fare * count;
+  });
+  
+  const discount = calculateFare(multiply, 0.1, function(total, rate) {
+    return total - (total * rate);
+  });
+  
+  console.log("5명 요금:", multiply + "원");
+  console.log("10% 할인 후:", discount + "원");
+}
+
+function exampleSix() {
+  function createStationChecker(targetStation) {
+    return function(currentStation) {
+      if (currentStation === targetStation) {
+        return targetStation + "역에 도착했습니다!";
+      } else {
+        return currentStation + "역 통과중...";
+      }
+    };
+  }
+  
+  const checkForDaejeon = createStationChecker("대전역");
+  
+  console.log(checkForDaejeon("판암"));
+  console.log(checkForDaejeon("대전역"));
+  console.log(checkForDaejeon("중앙로"));
+}
+
+function exampleSeven() {
+  const stationInfo = {
+    name: "시청",
+    index: 10,
+    transfer: false
+  };
+  
+  function processStation(station, callback) {
+    let message = station.name + "역 (";
+    message += station.index + 1 + "번째)";
+    
+    return callback(message, station.transfer);
+  }
+  
+  const result = processStation(stationInfo, function(msg, hasTransfer) {
+    if (hasTransfer) {
+      return msg + " - 환승가능";
+    } else {
+      return msg + " - 환승불가";
+    }
+  });
+  
+  console.log(result);
+}
+
+function exampleEight() {
+  function calculateProgress(current, total, callback) {
+    let percentage = (current / total) * 100;
+    return callback(Math.round(percentage));
+  }
+  
+  const currentStation = 7;
+  
+  const progress = calculateProgress(currentStation, daejeonLine1.stations.length, function(percent) {
+    return "전체 노선의 " + percent + "% 진행됨";
+  });
+  
+  console.log(progress);
+}
+
+function exampleNine() {
+  function transformStations(stations, callback) {
+    let newArray = [];
+    
+    for (let i = 0; i < stations.length; i++) {
+      newArray[i] = callback(stations[i], i);
+    }
+    
+    return newArray;
+  }
+  
+  const stationObjects = transformStations(daejeonLine1.stations.slice(0, 5), function(name, index) {
+    return {
+      id: index + 1,
+      name: name + "역",
+      zone: index < 7 ? "동구" : "서구"
+    };
+  });
+  
+  console.log("역 객체 배열:", stationObjects);
+}
+
+function exampleTen() {
+  const rideData = {
+    station: "정부청사",
+    time: "08:30",
+    passengers: 250
+  };
+  
+  function analyzeRide(data, timeCallback, crowdCallback) {
+    let timeResult = timeCallback(data.time);
+    let crowdResult = crowdCallback(data.passengers);
+    
+    return {
+      station: data.station,
+      rushHour: timeResult,
+      crowdLevel: crowdResult
+    };
+  }
+  
+  const analysis = analyzeRide(rideData, 
+    function(time) {
+      let hour = parseInt(time.split(":")[0]);
+      return (hour >= 7 && hour <= 9) || (hour >= 18 && hour <= 20);
+    },
+    function(count) {
+      if (count > 200) return "혼잡";
+      if (count > 100) return "보통";
+      return "여유";
+    }
+  );
+  
+  console.log("승차 분석:", analysis);
+}
+
+const examples = [exampleOne, exampleTwo, exampleThree, exampleFour, exampleFive, exampleSix, exampleSeven, exampleEight, exampleNine, exampleTen];
+
+for (let i = 0; i < examples.length; i++) {
+  examples[i]();
+  console.log("\n");
+}
