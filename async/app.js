509bc40da9fff5fd466f474a3f64705403f03f8d
csm <csm009177@gmail.com>
2025-08-08 18:01:30 +0900

    재귀함수를 활용한 이동

--- async/app.js
+++ async/app.js
@@ -1,6 +1,4 @@
-setTimeout(() => {
-  console.log("안녕 현준?");
-},1000);
-setTimeout(() => {
-  console.log("현준 욱순이 누구야?")
-},500);
+const run = setTimeout(() => {
+    console.log("시작!" );
+    
+}, 100);
