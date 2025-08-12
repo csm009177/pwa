91aeccf947d51f86ff4bd488c40b66c651fd3714
csm <csm009177@gmail.com>
2025-08-12 10:28:39 +0900

    전역변수, 전역변수_지역변수, 클로저

--- closure/state.js
+++ closure/state.js
@@ -8,8 +8,13 @@ function state() {
 
   // return toggle;
 
-  return function toggle() {
+  return function() {
     handle = !handle; // true가 아니다 => false
     return handle; // false를 반환
   }
 }
+
+
+const test = state();
+console.log(test()); // false
+console.log(test()); // true
