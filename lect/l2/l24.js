78d729d43f1f8e93272ad5e4b6b18281a8e586e3
csm <141980543+csm009177@users.noreply.github.com>
2025-08-17 23:41:03 +0900

    l24.js - 컴포넌트 이용하지 않고 이중반복문 돌기

--- lect/l2/l24.js
+++ lect/l2/l24.js
@@ -1,21 +1,50 @@
-var academyInfo = {
-  city: "대전",
-  district : "서구",
-  neighborhood : "둔산동"
-}
+var academies = [
+    {
+        city: "대전",
+        district: "서구",
+        neighborhood: "둔산동",
+        building: "오라클빌딩",
+        name: "그린아카데미아트학원",
+        branch: "대전둔산",
+        courses: ["웹개발", "앱개발", "데이터분석"]
+    },
+    {
+        city: "대전",
+        district: "중구",
+        neighborhood: "은행동",
+        building: "대전빌딩",
+        name: "그린아카데미아트학원",
+        branch: "대전중구",
+        courses: ["디자인", "마케팅", "프로그래밍"]
+    },
+    {
+        city: "서울",
+        district: "강남구",
+        neighborhood: "역삼동",
+        building: "테크노빌딩",
+        name: "그린아카데미아트학원",
+        branch: "서울강남",
+        courses: ["AI", "빅데이터", "클라우드"]
+    }
+];
 // 아래 부분 대괄호가 요번 주요 핵심!
 var addressComponents = [
-  academyInfo["city"],
-  academyInfo["district"],
-  academyInfo["neighborhood"]
-]
+  academies["city"],
+  academies["district"],
+  academies["neighborhood"],
+  academies["building"],
+  academies["name"],
+  academies["branch"],
+  academies["courses"]
+];
 
 var rangeValue = {
   start : 0,
-  end : 5
-}
+  end : academies.length,
+};
 
-for(var index = rangeValue["start"]; index < rangeValue["end"]; index++) {
-  // * rangeValue에 맞추어서 반복문 설정
-  console.log(addressComponents[index]); // ? 새로운 문법!(배열)
+for(let i=rangeValue.start; i<academies.length; i++){
+  for(let key in academies[i]){
+    console.log(academies[i][key]);
+  }
 }
