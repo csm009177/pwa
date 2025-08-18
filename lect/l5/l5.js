be13affbe095b75921d41ab64c076c80140f4517
csm <141980543+csm009177@users.noreply.github.com>
2025-08-18 23:29:22 +0900

    not-ops.html - 이름두개가 같은지 체크하는 조건문

--- lect/l5/l5.js
+++ lect/l5/l5.js
@@ -8,15 +8,6 @@ const students = [
     age: "111011110",
   },
 ];
-// ----------------------------------------------------------------
-function forLoopPrac() {
-  const end = 10;
-  for (let i = 0; i < end; i++) {
-    console.log("일단 만듦");
-  }
-}
-// ----------------------------------------------------------------
-// forLoopPrac();
 
 function 배열안에객체에접근() {
   for (let i = 0; i < students.length; i++) {
