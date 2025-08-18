c62d0665c932744e252e1f9af48069fdaba86ba4
csm <141980543+csm009177@users.noreply.github.com>
2025-08-18 23:27:06 +0900

    l5.js - 배열안에 객체를 넣는 이유는 키를 몰라도 접근하려고

--- lect/l5/l5.js
+++ lect/l5/l5.js
@@ -1,14 +1,29 @@
-const students = [{
-  name : "",
-  age : null,
-}]
+const students = [
+  {
+    name: "이름1",
+    age: "30003",
+  },
+  {
+    name: "이름2",
+    age: "111011110",
+  },
+];
 // ----------------------------------------------------------------
 function forLoopPrac() {
   const end = 10;
-  for(let i = 0; i < end; i++) {
-    console.log('일단 만듦');
+  for (let i = 0; i < end; i++) {
+    console.log("일단 만듦");
   }
 }
 // ----------------------------------------------------------------
-forLoopPrac();
+// forLoopPrac();
 
+function 배열안에객체에접근() {
+  for (let i = 0; i < students.length; i++) {
+    console.log(students[i]);
+    for(let key in students[i]){
+        console.log(students[i][key])
+    }
+  }
+}
+배열안에객체에접근();
