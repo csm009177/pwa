7d6039a23482eda75f79226f5af4241edf6c4ddc
csm <141980543+csm009177@users.noreply.github.com>
2025-08-18 23:42:42 +0900

    mkByWookjae.js - 입력받은 데이터와 조합하여 html파일 만들기

--- lect/l5/mkByWookjae.js
+++ lect/l5/mkByWookjae.js
@@ -1,12 +1,13 @@
 //* 구성 부분
 // ? 나 입력받는것을 하고 싶어.
 // ? 내 이름과 나이정도?
-const readline = require('readline');
+const readline = require("readline");
+const fs = require("fs");
 
 const inAndOut = {
   input: process.stdin,
-  output: process.stdout
-}
+  output: process.stdout,
+};
 const rl = readline.createInterface(inAndOut);
 
 // todo : -------------------------------------------
@@ -15,8 +16,23 @@ const rl = readline.createInterface(inAndOut);
 
 rl.question("니 이름뭐야? : ", (name) => {
   console.log(`안녕 ${name} !`);
+  fs.writeFileSync(
+    `${name}.html`,
+    `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>안녕 ${name} !</h1>
+</body>
+</html>`
+  );
+  console.log(`${name}.html 파일이 생성 또는 업데이트되었습니다.`);
   rl.close();
-})
+});
 
 // todo : -------------------------------------------
 //* 조정, 조종 controller
