8200ac0176eeae0d476212e68cdb26dc15b57172
csm <csm009177@gmail.com>
2025-08-19 09:50:29 +0900

    dynamic.js - 객체의 동적할당

--- lect/l6/dynamic.js
+++ lect/l6/dynamic.js
@@ -5,5 +5,23 @@ pokemon[index] = "피카츄";
 index = index + 1;
 pokemon[index] = "라이츄";
 
-console.log(pokemon);
-console.log(pokemon.length);
+// console.log(pokemon);
+// console.log(pokemon.length);
+
+const kong = {
+  name: "공욱재",
+  age: 26,
+  glassies: true,
+}
+console.dir(kong);
+kong.lunch = "김밥";
+console.dir(kong);
+
+const kokemon = [];
+let index2 = 0; 
+
+pokemon[index2] = "피카츄";
+pokemon[index2] = "라이츄";
+
+console.log(kokemon);
+console.log(kokemon.length);
