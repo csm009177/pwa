b646d4ade1a729f713b6a6ec1a8452db4d7b2060
csm <csm009177@gmail.com>
2025-08-19 10:04:35 +0900

    ./lect/l6/class.js - 정식 클래스 문법

---
+++ lect/l6/class.js
@@ -0,0 +1,21 @@
+class Test {
+  name = "공욱재";
+}
+//-----------------------------
+const example = {
+  name: "공욱재"
+}
+console.log(Test);
+const test = new Test
+console.log(test);
+console.log(example);
+
+// 정식 클래스문법
+class Test2 {
+  constructor() {
+    this.test = "공욱재";
+  }
+}
+
+const test2 = new Test();
+console.log(test2);
