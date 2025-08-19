79c23b5b7d8fe6aad67d10bf9d674ef7e3a95f53
csm <csm009177@gmail.com>
2025-08-19 10:58:45 +0900

    ./lect/l6/class-1.js - constructor를 사용한 클래스

--- lect/l6/class-1.js
+++ lect/l6/class-1.js
@@ -3,5 +3,14 @@ class 야체 {
   가격 = 0;
 }
 
-const 토마토 = new 야체('토마토', 1000)
+const 토마토 = new 야체('토마토', 5000);
 console.log(토마토)
+
+class 야체2 {
+  constructor(이름, 가격){
+    this.위름 = 이름;
+    this.가굑 = 가격;
+  }
+}
+const 오이 = new 야체2('오이', 1500);
+console.log(오이)
