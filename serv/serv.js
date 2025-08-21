562b0e9cb62abc8fdde7e163cf3b0fe931dfe555
csm <141980543+csm009177@users.noreply.github.com>
2025-08-21 23:17:19 +0900

    ./serv/serv.js - /join이라는 요청을 받으면 정보를 담을 json 파일을 자장

--- serv/serv.js
+++ serv/serv.js
@@ -3,15 +3,32 @@ const fs = require("fs");
 const port = 8080;
 
 const serv = http.createServer((req, res) => {
-  const fileName = __filename;
   if (req.url === "/") {
     fs.readFile("./serv/src/index.html", (err, data) => {
       res.end(data);
     });
   }
-  if(req.url === '/join'){
-    fs.writeFileSync('test.json', 'join', 'utf-8');
-    res.end( console.log('json 파일 생성완료'));
+  if(req.url === '/join'&& req.method === 'POST'){
+    let 받은데이터 = '';
+
+    req.on('data', (chunk) => {
+      받은데이터 += chunk.toString();
+    });
+
+    req.on('end', ()=> {
+      const 폼데이터 = {};
+      const 키값쌍들 = 받은데이터.split('&');
+      
+      for(let 한쌍 of 키값쌍들) {
+        const [키, 값] = 한쌍.split('=');
+        폼데이터[decodeURIComponent(키)] = decodeURIComponent(값 || '');
+      }      
+      console.log('받은 폼데이터:', 폼데이터);
+      
+      fs.writeFileSync('test.json', JSON.stringify(폼데이터, null, 2), 'utf-8');
+      console.log('json 파일 생성완료');
+      res.end('회원가입 완료!');
+    });
   }
 });
 
