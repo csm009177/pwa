23315cdbde15e227e02d01d96de159fa8adaade5
csm <csm009177@gmail.com>
2025-08-22 11:43:24 +0900

    serv/express.js - /join 요청을 받으면 무조건 test.json을 만들어준다

--- serv/express.js
+++ serv/express.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 const serv = express();
+const fs = require("fs");
 const port = 8080;
 
-serv.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+serv.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+serv.post("/join", (req, res) => {
+  fs.writeFileSync("test.json", "join", "utf-8");
 });
 
 serv.listen(port, () => {
-    console.log(`가동되었습니다 클릭하세요 => http://localhost:${port}`);
+  console.log(`가동되었습니다 클릭하세요 => http://localhost:${port}`);
 });
