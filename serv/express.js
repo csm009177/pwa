aa26a4d1ac54a82cc2b824aa12d8a3a75e9c11ab
csm <csm009177@gmail.com>
2025-08-22 11:59:15 +0900

    serv/express.js - /join 요청을 받으면 test.json에 데이터를 저장해주는 알바생

--- serv/express.js
+++ serv/express.js
@@ -8,7 +8,24 @@ serv.get("/", (req, res) => {
 });
 
 serv.post("/join", (req, res) => {
-  fs.writeFileSync("test.json", "join", "utf-8");
+  let 받은데이터 = "";
+
+  req.on("data", (chunk) => {
+    받은데이터 += chunk.toString();
+  });
+  req.on("end", () => {
+    const 폼데이터 = {};
+    const 키값쌍들 = 받은데이터.split("&");
+    for (let 인덱스 in 키값쌍들) {
+      const 한쌍 = 키값쌍들[인덱스];
+      const [키, 값] = 한쌍.split("=");
+      폼데이터[decodeURIComponent(키)] = decodeURIComponent(값 || "");
+    }
+    console.log("받은 폼데이터:", 폼데이터);
+    fs.writeFileSync("test.json", JSON.stringify(폼데이터, null, 2), "utf-8");
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");``
+    res.end("회원가입 완료!");
+  });
 });
 
 serv.listen(port, () => {
