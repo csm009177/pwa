52ca23fca7858e29cbc52aff3f8dee4944e33aba
csm <csm009177@gmail.com>
2025-08-22 09:30:33 +0900

    ./serv/serv.js - 같은 아이디는 만들면 안되겠죠?

--- serv/serv.js
+++ serv/serv.js
@@ -8,26 +8,37 @@ const serv = http.createServer((req, res) => {
       res.end(data);
     });
   }
-  if(req.url === '/join'&& req.method === 'POST'){
-    let 받은데이터 = '';
+  if (req.url === "/join" && req.method === "POST") {
+    let 받은데이터 = "";
 
-    req.on('data', (chunk) => {
+    req.on("data", (chunk) => {
       받은데이터 += chunk.toString();
     });
 
-    req.on('end', ()=> {
+    req.on("end", () => {
       const 폼데이터 = {};
-      const 키값쌍들 = 받은데이터.split('&');
-      
-      for(let 한쌍 of 키값쌍들) {
-        const [키, 값] = 한쌍.split('=');
-        폼데이터[decodeURIComponent(키)] = decodeURIComponent(값 || '');
-      }      
-      console.log('받은 폼데이터:', 폼데이터);
-      
-      fs.writeFileSync('test.json', JSON.stringify(폼데이터, null, 2), 'utf-8');
-      console.log('json 파일 생성완료');
-      res.end('회원가입 완료!');
+      const 키값쌍들 = 받은데이터.split("&");
+
+      for (let 한쌍 of 키값쌍들) {
+        const [키, 값] = 한쌍.split("=");
+        폼데이터[decodeURIComponent(키)] = decodeURIComponent(값 || "");
+      }
+      console.log("받은 폼데이터:", 폼데이터);
+
+      const 폼데이터에있는ID = 폼데이터["id"];
+      console.log(폼데이터에있는ID);
+
+      if (fs.existsSync(폼데이터에있는ID + ".json")) {
+        console.log("같은 id의 파일이 존재합니다");
+        res.setHeader("Content-Type", "text/plain; charset=utf-8");
+        res.end("이미 존재하는 ID입니다.");
+        return;
+      } else {
+        fs.writeFileSync( 폼데이터에있는ID + ".json", JSON.stringify(폼데이터, null, 2), "utf-8" );
+        console.log("json 파일 생성완료");
+        res.setHeader("Content-Type", "text/plain; charset=utf-8");
+        res.end("회원가입 완료!");
+      }
     });
   }
 });
