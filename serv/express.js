f3195e1b2a4d5a18e38befe1b27ae87a0186ad4c
csm <csm009177@gmail.com>
2025-08-22 10:55:33 +0900

    serv\express.js - 무조건 문서를 가져다 주는 알바생

--- serv/express.js
+++ serv/express.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
+// 조금만 말해도 잘 알아듣는거 같습니다
+const serv = app.use(express.static(__dirname + '/src')); 
 
-app.listen(port, () => {
+serv.listen(port, () => {
     console.log(`가동되었습니다 클릭하세요 => http://localhost:${port}`);
 });
