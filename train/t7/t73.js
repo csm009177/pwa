369bd4d67d4ba5241efd49095d20af47843553ce
csm <csm009177@gmail.com>
2025-08-25 09:03:56 +0900

    t73.js - 연습 시작

---
+++ train/t7/t73.js
@@ -0,0 +1,10 @@
+const htmlData = {
+    tags: ["div", "h1", "p", "span", "button", "input", "img", "a"],
+    colors: ["red", "blue", "green", "yellow", "purple", "orange", "pink", "black"],
+    sizes: [16, 24, 18, 14, 20, 12, 32, 28],
+    positions: ["static", "relative", "absolute", "fixed", "sticky"],
+    displays: ["block", "inline", "flex", "grid", "none"],
+    borders: ["solid", "dashed", "dotted", "double"],
+    fonts: ["Arial", "Georgia", "Times", "Helvetica", "Verdana"],
+    weights: [100, 200, 300, 400, 500, 600, 700, 800, 900]
+};
