f25ecaa74da235c96de5cd3fc73c7cfbf46167aa
csm <141980543+csm009177@users.noreply.github.com>
2025-08-26 23:33:56 +0900

    .editorconfig - 저장 시 자동으로 LF로 변환됨 / .gitattributes - 자동 변환설정

--- nr/ns/src/main.ts
+++ nr/ns/src/main.ts
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin : 'http://localhost:3000',
+    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
