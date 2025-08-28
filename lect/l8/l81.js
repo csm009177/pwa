745f9ddc68197ddc0047657f9bc9f7078e08d48f
csm <csm009177@gmail.com>
2025-08-28 18:09:25 +0900

    l81.js - 객체로 만든 문서를 서빙까지 하기

--- lect/l8/l81.js
+++ lect/l8/l81.js
@@ -1,9 +1,15 @@
+const http = require("http");
 const fs = require("fs");
+const port = 5050;
 
 const htmlDocument = {
-  htmlTag: { start: "<!DOCTYPE html>", end: "</html>" },
+  htmlTag: { start: "<!DOCTYPE html><html lang='ko'>", end: "</html>" },
   head: { start: "<head>", end: "</head>" },
+  meta: { start: "<meta charset='UTF-8'>", end: "" },
   body: { start: "<body>", end: "</body>" },
+  div: { start: "<div>", end: "</div>" },
+  p: { start: "<p>", end: "</p>" },
+  span: { start: "<span>", end: "</span>" },
 };
 
 function htmlMaker(받은문서) {
@@ -12,21 +18,29 @@ function htmlMaker(받은문서) {
   ${받은문서.htmlTag.start}
   ${받은문서.head.start}
   ${받은문서.head.end}
+  ${받은문서.meta.start}
+  ${받은문서.meta.end}
+  ${'안녕하세요'}
+
   ${받은문서.body.start}
   ${받은문서.body.end}
   ${받은문서.htmlTag.end}
 `;
-  fs.writeFileSync(__dirname + '/' + "index.html", result);
+  fs.writeFileSync(__dirname + "/" + "index.html", result);
   return result;
 }
-
 htmlMaker(htmlDocument);
 
-function htmlreader(){
-    const data = fs.readFileSync("index.html", "utf-8");
-    console.log(data);
-    console.log('http://127.0.0.1:5500/index.html');
-
+function htmlReader() {
+  const data = fs.readFileSync(__dirname + "/" + "index.html", "utf-8");
+  return data;
 }
 
-htmlreader();
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(htmlReader());
+  })
+  .listen(port, () => {
+    console.log(`Server running at http://127.0.0.1:${port}/`);
+  });
