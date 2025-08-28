50b062840bd933104363c0896e96652e2ea8bb7a
csm <csm009177@gmail.com>
2025-08-28 12:56:04 +0900

    l8 - 모든 웹 요소가 결국 JavaScript 객체

---
+++ lect/l8/l8-class_syntax.js
@@ -0,0 +1,22 @@
+class PracHTMLObject {
+  head() {
+    return {
+      meta: [
+        {charset:"UTF-8"},
+        {name:"viewport", content:"width=device-width, initial-scale=1.0"},
+      ],
+      title: {
+        textContent : "문서의 제목이오",
+      }
+    }
+  }
+  body() {
+    return {
+
+    }
+  }
+}
+
+const test = new PracHTMLObject();
+console.log(test.head());
+console.log(test.body());
---
+++ lect/l8/l8-datastcture.js
@@ -0,0 +1,16 @@
+const HTML = {
+  head: {
+    meta: [
+      {charset:"UTF-8"},
+      {name:"viewport", content:"width=device-width, initial-scale=1.0"},
+    ],
+    title: {
+      textContent : "문서의 제목이오",
+    }
+  },
+  body: {
+
+  }
+}
+
+console.log(HTML);
